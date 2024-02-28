d68e8372d39a4346a5f6ffe61ffc2cd491956319
Kill-JoyCoder <ankurgohain2005@gmail.com>
2024-02-29 00:51:03 +0530

    Commit with a basic input output in C++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    cin>>a; // whitespace around cin and cout will result in errors, so don't put any :P
+    cout<<a;
+    return 0;
+}
